5d17fb5a1e330331e6ce37c73453c2cb04a78fd1
Yifan Gao <git@gaoyifan.com>
2014-12-27 08:28:05 +0800

    fix main.local filename in env.sh

--- config/env.sh
+++ config/env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export WORKDIR=/srv/startup
-export MAIN_ENTRY="${WORKDIR}/main.sh"
+export MAIN_ENTRY="${WORKDIR}/main.local"
 export U6100_DEV="/dev/ttyUSB3"
 export PL2303_DEV="/dev/ttyUSB0"
 export LOG_DIR="/var/log/dronevery"
