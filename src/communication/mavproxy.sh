70785aa0291e59a82bf92a366b2381682fff86c4
Yifan Gao <git@gaoyifan.com>
2014-12-31 02:34:03 +0800

    modify mavproxy log for add mode

--- src/communication/mavproxy.sh
+++ src/communication/mavproxy.sh
@@ -3,6 +3,6 @@
 function init_mavproxy()
 {
 	debug_msg "mavlink_async"
-	nohup python /home/xuhao/mavasync/mavlink_async.py >> ${LOG_MAVPROXY} &	
+	nohup python /home/xuhao/mavasync/mavlink_async.py $>> ${LOG_MAVPROXY} &
 }
 
