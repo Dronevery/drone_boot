c3e62bc0e8d7eb5d4c4f71b04e913c48c8a5929e
Yifan Gao <git@gaoyifan.com>
2014-12-31 01:09:21 +0800

    add debug msg

--- src/net/U6100.sh
+++ src/net/U6100.sh
@@ -2,10 +2,13 @@
 
 function init_U6100()
 {
+	debug_msg "U6100(ppp) starting..."
 	local DEV="$1"
 	while [ ! $ip ];do
+		debug_msg "U6100 try to connecting..."
     	local ip=$(ip addr show dev ppp0 | awk '{if($1=="inet") print $2}' | awk 'BEGIN{FS="/"}{print $1}' | head -n 1)
     	if [ `ps aux | grep pppd | wc -l` = 1 ];then
+    		debug_msg "U6100 call pppd"
 	        /usr/sbin/pppd "${DEV}" connect "chat -v -f /srv/DTR-3G/chatscript "
 	    fi
     	sleep 3
