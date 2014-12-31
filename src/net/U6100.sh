bc2e87dd4eedad9d4f33078169d3d00efda33052
Yifan Gao <git@gaoyifan.com>
2014-12-31 19:13:13 +0800

    adjust fotmat

--- src/net/U6100.sh
+++ src/net/U6100.sh
@@ -9,12 +9,12 @@ function init_U6100()
 	debug_msg "U6100 dev file: $DEV"
 	while [ ! $ip ];do
 		debug_msg "U6100 try to connecting..."
-    	local ip=$(ip addr show dev ppp0 2>/dev/null | awk '{if($1=="inet") print $2}' | awk 'BEGIN{FS="/"}{print $1}' | head -n 1)
-    	if [ `ps aux | grep pppd | wc -l` = 1 ];then
-    		debug_msg "U6100 call pppd"
-	        /usr/sbin/pppd "${DEV}" connect "chat -v -f /srv/DTR-3G/chatscript "
-	    fi
-    	sleep 3
+		local ip=$(ip addr show dev ppp0 2>/dev/null | awk '{if($1=="inet") print $2}' | awk 'BEGIN{FS="/"}{print $1}' | head -n 1)
+		if [ `ps aux | grep pppd | wc -l` = 1 ];then
+			debug_msg "U6100 call pppd"
+			/usr/sbin/pppd "${DEV}" connect "chat -v -f /srv/DTR-3G/chatscript "
+		fi
+		sleep 3
 	done;
 	
 	export UNICOM_IFACE="ppp0"
