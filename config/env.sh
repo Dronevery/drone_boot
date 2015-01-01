509ef88059287c09b433432c8ef2b92595b455f0
Yifan Gao <git@gaoyifan.com>
2015-01-01 22:41:07 +0800

    use fixed dev file for U6100

--- config/env.sh
+++ config/env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export WORKDIR=/srv/drone_boot
 export MAIN_ENTRY="${WORKDIR}/main.local"
-#export U6100_DEV="/dev/ttyUSB3"
-export PL2303_DEV="/dev/ttyUSB0"
+export U6100_DEV="/dev/ttyUSB.U6100"
+export PL2303_DEV="/dev/ttyUSB.PIX"
 export LOG_DIR="/var/log/drone"
 export LOG_MAIN="${LOG_DIR}/drone"
 export LOG_MAIN_LEVEL=3
--- src/net/U6100.sh
+++ src/net/U6100.sh
@@ -4,15 +4,15 @@ source ${WORKDIR}/dev/dev.sh
 function init_U6100()
 {
 	debug_msg "U6100(ppp) starting..."
-	local DEV
-	DEV=$(usb2dev 1c9e 9603 | awk '{print $3}')
-	debug_msg "U6100 dev file: $DEV"
+	#local DEV
+	#DEV=$(usb2dev 1c9e 9603 | awk '{print $3}')
+	#debug_msg "U6100 dev file: $DEV"
 	while [ ! $ip ];do
 		debug_msg "U6100 try to connecting..."
 		local ip=$(ip addr show dev ppp0 2>/dev/null | awk '{if($1=="inet") print $2}' | awk 'BEGIN{FS="/"}{print $1}' | head -n 1)
 		if [ `ps aux | grep pppd | wc -l` = 1 ];then
 			debug_msg "U6100 call pppd"
-			/usr/sbin/pppd "${DEV}" connect "chat -v -f /srv/DTR-3G/chatscript "
+			/usr/sbin/pppd "${PL2303_DEV}" connect "chat -v -f /srv/DTR-3G/chatscript "
 		fi
 		sleep 3
 	done;
