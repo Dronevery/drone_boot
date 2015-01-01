226859d530f8e1000eb5d7697ee5742afbd8e836
Yifan Gao <git@gaoyifan.com>
2015-01-02 01:28:15 +0800

    fix serial device of U6100

--- src/net/U6100.sh
+++ src/net/U6100.sh
@@ -12,7 +12,7 @@ function init_U6100()
 		local ip=$(ip addr show dev ppp0 2>/dev/null | awk '{if($1=="inet") print $2}' | awk 'BEGIN{FS="/"}{print $1}' | head -n 1)
 		if [ `ps aux | grep pppd | wc -l` = 1 ];then
 			debug_msg "U6100 call pppd"
-			/usr/sbin/pppd "${PL2303_DEV}" connect "chat -v -f /srv/DTR-3G/chatscript "
+			/usr/sbin/pppd "${U6100_DEV}" connect "chat -v -f /srv/DTR-3G/chatscript "
 		fi
 		sleep 3
 	done;
