d3f919fa060fcf4eb7cba4ac68076e81a02aaf9e
Yifan Gao <git@gaoyifan.com>
2015-01-05 07:18:31 +0800

    add preference for ip rule

--- src/net/route.sh
+++ src/net/route.sh
@@ -3,13 +3,13 @@
 function route_3g()
 {
 	if [ $IFACE_UNICOM ];then
-		ip rule add from ${IP_UNICOM} lookup 1000
+		ip rule add from ${IP_UNICOM} lookup 1000 pref 1000
 		ip route replace ${IP_SERVER} dev ${IFACE_UNICOM} table 1000
 		ip route replace ${IP_SERVER} dev ${IFACE_UNICOM} table main
 	fi
 
 	if [ $IFACE_TELECOM ];then
-		ip rule add from ${IP_TELECOM} lookup 1001
+		ip rule add from ${IP_TELECOM} lookup 1001 pref 1001
 		ip route replace ${IP_SERVER} dev ${IFACE_TELECOM} table 1001
 		ip route replace ${IP_SERVER} dev ${IFACE_TELECOM} table main
 	fi
