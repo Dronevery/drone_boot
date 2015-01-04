502f66bbf4969d2134924bd22cc96e992b199a39
Yifan Gao <git@gaoyifan.com>
2015-01-05 07:15:14 +0800

    fix function ipaddress

--- src/net/ip.sh
+++ src/net/ip.sh
@@ -2,5 +2,5 @@
 function ipaddress()
 {
 	local interface=$1
-	echo ip addr show dev $interface 2>/dev/null | awk '{if($1=="inet") print $2}' | awk 'BEGIN{FS="/"}{print $1}' | head -n 1
+	echo `ip addr show dev $interface 2>/dev/null | awk '{if($1=="inet") print $2}' | awk 'BEGIN{FS="/"}{print $1}' | head -n 1`
 }
